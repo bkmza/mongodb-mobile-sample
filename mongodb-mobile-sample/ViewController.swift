597d82168f65a22f1b7d882bbcf082d610b04ade
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2018-11-23 16:33:20 +0300

    Initial Commit

---
+++ mongodb-mobile-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mongodb-mobile-sample
+//
+//  Created by Ilya Usikov on 11/23/18.
+//  Copyright © 2018 Ilya Usikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
